21c0cabd3b8793345d6edf645cace8dd13b4b851
FootsoreFlame <82735577+FootsoreFlame@users.noreply.github.com>
2023-09-10 00:09:57 -0700

    change font button

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-<button id="myButton">Click Me!</button>
+var fonts = [
+    'Times New Roman',
+    'Helvetica, Arial, sans-serif', 
+    'Brush Script MT',
+    'Arial, sans-serif', 
+    'Courier New, Courier, monospace',
+    'Rotis, sans-serif', 
+    'Souvenir, sans-serif', 
+    'Grunge Fonts', 
+    'Avant Garde, sans-serif', 
+    'Gill Sans, sans-serif', 
+    'Comic Sans MS, cursive'
+];
 
-<script>
-  document.getElementById('myButton').addEventListener('click', function () {
-    alert('You clicked the button!')
-  })
-</script>
+var currentFontIndex = 0;
+
+document.getElementById('myButton').addEventListener('click', function () {
+    var textElement = document.getElementById('text');
+
+      textElement.classList.remove('font-class');
+
+      currentFontIndex = (currentFontIndex + 1) % fonts.length;
+
+       textElement.style.fontFamily = fonts[currentFontIndex];
+
+    alert('Font Changed!');
+});
